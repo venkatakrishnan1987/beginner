35a815d3e7a263741d0e40add00dcae9ac01b11c
Baraa Khatib Salkini <69188047+DataWithBaraa@users.noreply.github.com>
2025-07-31 11:17:03 +0200

    Update 11_conditional_statements.py

--- scripts/11_conditional_statements.py
+++ scripts/11_conditional_statements.py
@@ -45,3 +45,19 @@ elif score >= 60:
 else:
     print("F")
 
+
+score = 50
+submitted_project = True
+if score >= 90 and submitted_project:
+    print("A+")
+elif score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60 or submitted_project:
+    print("D")
+else:
+    print("F")
+
