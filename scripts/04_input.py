1e061f5b182f89b8c0b6648fe013640551f0c737
Baraa Khatib Salkini <69188047+DataWithBaraa@users.noreply.github.com>
2025-07-31 13:12:16 +0200

    Update 04_input.py

--- scripts/04_input.py
+++ scripts/04_input.py
@@ -1,3 +1,16 @@
+# ================================================================================
+# VARIABLES & USER INPUT
+# ----------------------------------------
+# This section shows how to accept user input using the `input()` function.
+# We then combine it with a predefined variable to display a complete sentence.
+# ================================================================================
+
+
+# Dynamic Value: Ask the user to enter their name and store it in a variable.
 name = input("Enter Your Name:")
+
+# Hardcoded value
 country = "Germany"
+
+# Combine both variables
 print(name, "comes from", country)
