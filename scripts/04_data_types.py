4afc23a6532f9a405de8c99af77e4e1f6d6fd509
Baraa Khatib Salkini <69188047+DataWithBaraa@users.noreply.github.com>
2025-07-31 08:47:56 +0200

    Update 05_data_types.py

--- scripts/04_data_types.py
+++ scripts/04_data_types.py
@@ -1,1 +1,34 @@
-tbd
+# Data Types
+a = 10        # int
+b = 3.15      # float
+c = "Hello"   # str
+d = 'Hi'      # str
+e = "1234"    # str (looks like a number, but it's a string)
+f = True      # bool
+g = False     # bool
+h = None      # NoneType
+i = ""        # str - blank (empty string)
+j = " "       # str - contains one empty space
+
+
+text = "hi"
+number = 10
+
+print(type(text))
+print(type(number))
+
+
+text = "hi"
+number = 10
+
+print(text.upper())           # Converts text to uppercase -> "HI"
+print(number.bit_length())    # Returns number of bits to represent 10 -> 4
+print(text.bit_length())      # ❌ Error! Strings don't have bit_length()
+
+
+Create 5 variables – each with a different data type:
+- Your age
+- Your height (with decimals)
+- Your name
+- Are you a student?
+- Something with no value yet
